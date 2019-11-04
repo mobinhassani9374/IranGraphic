f9b65f13490be09fde452967fd0a19642339b944
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-11-04 20:12:11 +0330

    add Utility

---
+++ SeratGraphic.Utility/ServiceResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeratGraphic.Utility
+{
+    public class ServiceResult
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public static ServiceResult Okay()
+        {
+            return new ServiceResult
+            {
+                IsSuccess = true,
+                Message = "عملیات با موفقیت صورت گرفت"
+            };
+        }
+        public static ServiceResult Error()
+        {
+            return new ServiceResult
+            {
+                IsSuccess = false,
+                Message = "در انجام عملیات خطایی صورت گرفت مجددا تلاش کنید"
+            };
+        }
+        public static ServiceResult Okay(string message)
+        {
+            return new ServiceResult
+            {
+                IsSuccess = true,
+                Message = message
+            };
+        }
+        public static ServiceResult Error(string errorMessage)
+        {
+            return new ServiceResult
+            {
+                IsSuccess = false,
+                Message = errorMessage
+            };
+        }
+    }
+}
